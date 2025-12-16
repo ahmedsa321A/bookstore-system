75b021812788030d2e72483ece05d6d422cefa51
enghazem <hazembarakat012@gmail.com>
2025-12-16 17:11:06 +0200

    Edit authme route

--- client/book_store/src/pages/Unauthorized.tsx
+++
@@ -1,8 +0,0 @@
-// src/pages/Unauthorized.tsx
-export const Unauthorized = () => {
-    return (
-        <div className="min-h-screen flex items-center justify-center">
-            <h1 className="text-3xl font-bold text-red-600">403 - Unauthorized</h1>
-        </div>
-    );
-};
