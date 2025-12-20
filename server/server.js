25f7424dc9b9fbd6d57650a363088f4ae5808f63
Adham-zakaria <es-adham.zakaria2027@alexu.edu.eg>
2025-12-20 02:53:46 +0200

    add the admin routes to the server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const authRoutes = require('./routes/authRoutes'); 
 const cartRoutes = require('./routes/cartRoutes');
 const bookRoutes = require('./routes/bookRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
@@ -21,13 +22,14 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/auth', authRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/books', bookRoutes);
+app.use('/api/admin', adminRoutes);
 
-//handle undefined for book routes
+//handle undefined routes
 app.use((req, res) => {
     res.status(404).json({ message: "Route not found" });
 });
 
-// handle throw from the book controller
+// handle throw from try catch blocks
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: err.message });
