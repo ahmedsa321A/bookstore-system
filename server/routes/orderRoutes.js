037c2c7713b362d5a00d3e7d318ac6a905955f8c
enghazem <hazembarakat012@gmail.com>
2025-12-26 16:20:28 +0200

    changes

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,8 +1,5 @@
 const express = require('express');
-<<<<<<< Updated upstream
 const { checkout, getCustomerOrderHistory } = require('../controllers/orderController');
-=======
-const { checkout } = require('../controllers/orderController');
 const {
     getAllCustomerOrders,
     updateCustomerOrderStatus,
@@ -11,15 +8,20 @@ const {
     placePublisherOrder,
     confirmPublisherOrder
 } = require('../controllers/orderControllerAdmin');
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
 const verifyToken = require('../middleware/verifyToken');
 const router = express.Router();
 
 router.post('/checkout', verifyToken, checkout);
 router.get('/getCustomerOrderHistory', verifyToken, getCustomerOrderHistory);
+
+// Admin Routes
+router.get('/admin/all', verifyToken, getAllCustomerOrders);
+router.put('/admin/status/:id', verifyToken, updateCustomerOrderStatus);
+router.get('/admin/low-stock', verifyToken, getLowStockBooks);
+router.get('/admin/publisher-orders', verifyToken, getAllPublisherOrders);
+router.post('/admin/publisher-order', verifyToken, placePublisherOrder);
+router.put('/admin/publisher-order/:id/confirm', verifyToken, confirmPublisherOrder);
 
 // Admin Routes
 router.get('/admin/all', verifyToken, getAllCustomerOrders);
